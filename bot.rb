f8cfd55afd97f2dc39d5e598babd0b7f8c3f895f
qpingu <qpingu@gmail.com>
2010-12-03 21:59:16 -0500

    The vhost line will not work when testing locally, please uncomment it.
    Signed-off-by: pingu <qpingu@gmail.com>

--- bot.rb
+++ bot.rb
@@ -12,17 +12,18 @@ mainbot = Thread.new do
     configure do |c|
       c.nick = "IRCCompanionBot"
       c.server = "irc.gamesurge.net"
+      c.vhost = "zomgbbq.com"
       c.plugins.plugins = [ Pug, Quitter ]
       c.channels = [ "#tf2.pug.na.beta" ]
       c.verbose = true
     end
   end
-
+  
   MasterMessenger.instance.add bot
   bot.start
 end
 
-2.times do |i|
+0.times do |i|
   sleep(30)
 
   Thread.new do
