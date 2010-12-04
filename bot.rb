50849bf3b59b9233ed4d7e7ec07f0737854e21f1
qpingu <qpingu@gmail.com>
2010-12-04 00:00:51 -0500

    Changing up everything so your merges fail, eat that!
    Signed-off-by: pingu <qpingu@gmail.com>

--- bot.rb
+++ bot.rb
@@ -12,11 +12,11 @@ require './masterMessenger.rb'
 mainbot = Thread.new do
   bot = Cinch::Bot.new do
     configure do |c|
-      c.server = Variables::Irc_server
-      c.port = Variables::Irc_port
-      c.vhost = Variables::Irc_vhost
-      c.nick = Variables::Nick_bot
-      c.channels = [ Variables::Irc_channel ]
+      c.server = Const::Irc_server
+      c.port = Const::Irc_port
+      c.vhost = Const::Irc_vhost
+      c.nick = Const::Nick_bot
+      c.channels = [ Const::Irc_channel ]
       
       c.plugins.plugins = [ Pug, Quitter ]
       c.verbose = false
@@ -27,11 +27,11 @@ mainbot = Thread.new do
   bot.start
 end
 
-Variables::Messenger_count.times do |i|
+Const::Messenger_count.times do |i|
   sleep(30)
 
   Thread.new do
-    bot = Summer::Connection.new(Variables::Irc_server, Variables::Irc_port, "#{ Variables::Nick_messenger }#{ i }", Variables::Irc_channel, Variables::Irc_vhost)
+    bot = Summer::Connection.new(Const::Irc_server, Const::Irc_port, "#{ Const::Nick_messenger }#{ i }", Const::Irc_channel, Const::Irc_vhost)
       
     MasterMessenger.instance.add bot
     bot.start
