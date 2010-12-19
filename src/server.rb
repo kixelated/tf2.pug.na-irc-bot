12d7d31db117deeb363a0a5e45463b71d6a8caa8
qpingu <qpingu@gmail.com>
2010-12-19 13:11:02 -0500

    Server.rb, back from the dead.

---
+++ src/server.rb
@@ -0,0 +1,58 @@
+require 'rcon'
+
+class Server
+  attr_reader :name, :ip, :port, :password, :rcon
+  
+  def initialize name, ip, port, password, rcon
+    @name = name
+    @ip = ip
+    @port = port
+    @password = password
+    @rcon = rcon 
+
+    @connected = false
+  end
+  
+  #establish connection to server and auth
+  def connect 
+    @conn = RCon::Query::Source.new(ip, port)
+    @connected = @conn.auth rcon
+  end
+  
+  #execute any command passed
+  def command cmd
+    return unless connected? 
+    @conn.command cmd
+  end
+  
+  def cvar name
+    return unless connected?
+    @conn.cvar name
+  end
+
+  #change map
+  def clvl map
+    command "changelevel #{ map }"
+  end
+  
+  def cpswd pswd
+    command "password #{ pswd }"
+  end
+  
+  def connected?
+    @connected
+  end
+  
+  def in_use?
+    command("status") =~ /players : (\S+) /
+    return $1.to_i > const["settings"]["used"]
+  end
+  
+  def connect_info
+    "connect #{ @ip }:#{ @port }; password #{ @password }"
+  end
+  
+  def to_s
+    @name
+  end
+end
