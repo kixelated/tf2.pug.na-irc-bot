d41d48cd075d30f4ad2748d69903bf50091ff57f
qpingu <qpingu@gmail.com>
2010-12-21 00:45:44 -0500

    Spelled through incorrectly.

--- src/model/match.rb
+++ src/model/match.rb
@@ -4,6 +4,6 @@ class Match < ActiveRecord::Base
   has_and_belongs_to_many :teams # TODO: Create join table.
     
   has_many :players
-  has_many :users, :though => :players
-  has_many :stats, :though => :players
+  has_many :users, :through => :players
+  has_many :stats, :through => :players
 end
--- src/model/team.rb
+++ src/model/team.rb
@@ -9,8 +9,8 @@ class Team < ActiveRecord::Base
   has_and_belongs_to_many :users # TODO: Create join table.
   
   has_many :players
-  has_many :stats, :though => :players
-  has_many :matches, :though => :players
+  has_many :stats, :through => :players
+  has_many :matches, :through => :players
 
   attr_accessor :captain, :colour, :signups
   
