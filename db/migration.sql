51a73d738e0162def6119710d7f841b1f36f6ecb
qpingu <qpingu@gmail.com>
2011-01-16 01:28:12 -0500

    Updated the schema again.

--- db/migration.sql
+++ db/migration.sql
@@ -3,13 +3,6 @@ CREATE TABLE "signups" (
   "tfclass_id" INTEGER
 );
 CREATE INDEX "signups_player" ON "signups" ("player_id");
-CREATE TABLE "picks" (
-  "id" INTEGER PRIMARY KEY AUTOINCREMENT,
-  "player_id" INTEGER,
-  "tfclass_id" INTEGER
-);
-CREATE INDEX "picks_player" ON "picks" ("player_id");
-INSERT INTO "picks" ("player_id", "tfclass_id") SELECT "player_id", "tfclass_id" FROM "stats";
-DROP TABLE "stats";
+ALTER TABLE "stats" RENAME TO "picks";
 CREATE INDEX "players_match" ON "players" ("match_id");
 CREATE INDEX "players_team" ON "players" ("team_id");
