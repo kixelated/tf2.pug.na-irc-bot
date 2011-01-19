f072d22a042d914c3071311e9121f6e27449235d
qpingu <qpingu@gmail.com>
2011-01-19 00:14:21 -0500

    Fixed a minor formatting bug.

--- src/logic/picking.rb
+++ src/logic/picking.rb
@@ -47,7 +47,7 @@ module PickingLogic
 
     # Display all of the players
     output = @signups.keys.collect { |player| "(#{ lookup_i[player] }) #{ player }" }
-    notice current_captain, "#{ bold "all: " } #{ output * ", " }"
+    notice current_captain, "#{ bold rjust("all: ") } #{ output * ", " }"
 
     # Displays the classes that are not yet full for this team
     classes_needed(current_team.get_classes).each do |clss, count| # logic/players.rb
