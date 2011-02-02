383aa71b9082e4d59b5bf33aa4f80bffeace48c6
qpingu <qpingu@gmail.com>
2011-02-02 18:22:17 -0500

    Removed the migration file.

--- db/migration.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE "signups" (
-  "player_id" INTEGER,
-  "tfclass_id" INTEGER
-);
-CREATE INDEX "signups_player" ON "signups" ("player_id");
-ALTER TABLE "stats" RENAME TO "picks";
-CREATE INDEX "players_match" ON "players" ("match_id");
-CREATE INDEX "players_team" ON "players" ("team_id");
