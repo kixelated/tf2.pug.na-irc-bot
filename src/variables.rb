cc79014a927b2175cbd6a1bb1713398cc6ab087a
qpingu <qpingu@gmail.com>
2011-03-25 01:45:29 -0400

    Cleaned up the constants file and variables.rb

--- src/variables.rb
+++ src/variables.rb
@@ -9,43 +9,28 @@ module Variables
     DataMapper.finalize
     DataMapper.auto_migrate!
     
+    state "waiting"
+    
     @map = next_map
-  
-    @signups = {}
-    @auth = {}
+    
+    @users = {}
     @spoken = {}
-    @afk = []
     
     @toadd = {}
     @toremove = []
-
-    @teams = []
-    @pick_order = []
-    @lookup = {}
     
-    @state = const["states"]["waiting"]
-    @pick = 0
-
     @show_list = 0
   end
   
   def end_game
-    @teams.clear
-    @lookup.clear
-
     state "waiting"
-    @pick = 0
-    @pick_order = []
     
-    @auth.reject! { |k, v| !@signups.key? k }
+    @map = next_map
+    
+    @users.reject! { |k, v| !@signups.key? k }
     @spoken.reject! { |k, v| !@signups.key? k }
     
-    @toadd.each { |nick, classes| add_player User(nick), classes }
-    @toremove.each { |nick| remove_player User(nick) }
-    
-    @toadd.clear
-    @toremove.clear
-
-    @map = next_map
+    @toadd.reject! { |nick, classes| add_player User(nick), classes }
+    @toremove.reject! { |nick| remove_player User(nick) }
   end
 end
